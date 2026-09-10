7ac830c4a9348df102048ea4aadbe04e4628fa80
noreplydev <contact.cristiansanchez@gmail.com>
2026-09-11 01:14:27 +0200

    add ArrowRightIcon component

---
+++ icons/ArrowRightIcon.tsx
@@ -0,0 +1,7 @@
+export default function ArrowRightIcon({ width = 16, height = 16, className = '' }: { width?: number; height?: number; className?: string }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" className={className}>
+      <path d="M5 12h14M13 6l6 6-6 6" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
